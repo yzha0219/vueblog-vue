c8a12a733ac7b31f0cdd452c912233aac24aa854
John Doe <johndoe@hp.com>
2020-07-26 23:03:40 +0800

    Login Page (Not Completed)

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import Element from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
 import axios from 'axios'
+import './axios.js' // 请求拦截
 
 Vue.prototype.$axios = axios //
 
