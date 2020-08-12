25db4e8a50d733c2682091b56784565ca2de0ea3
John Doe <johndoe@hp.com>
2020-08-12 16:50:49 +0800

    Email format validation

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -64,6 +64,14 @@
                     callback();
                 }
             };
+            var checkEmail = (rule, value, callback) => {
+              const regEmail = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+              if(regEmail.test(this.ruleForm.email)) {
+                callback();
+              } else {
+                callback(new Error('The syntax of email is invalid!'));
+              }
+            }
             return {
                 ruleForm: {
                     username:'',
@@ -86,6 +94,7 @@
                         { validator: validatePass2, trigger: 'blur' }
                     ],
                     email: [
+                        { validator: checkEmail, trigger: 'blur'},
                         { required: true, message: "Please input email!", trigger: 'blur' }
                     ]
                 }
